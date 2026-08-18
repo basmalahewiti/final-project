af3f941c43be5235fd6786a5c286990a7d4f5491
basmalahewiti <basmala.m.desouky@gmail.com>
2026-08-18 12:34:25 +0300

    Add files via upload

---
+++ hobby.js
@@ -0,0 +1,72 @@
+let hobbies = [
+    {
+        name: "Drawing", mood: ["happy", "calm", "bored", "stressed"],
+        description: "A creative hobby that lets you express yourself and relax.",
+        img : "https://img.magnific.com/free-photo/close-up-artist-using-brush_1098-2209.jpg?semt=ais_test_b&w=740&q=80",
+        benefits :"Creativity, relaxation and self-expression", need:"Paper, pencil or drawing apps",
+        link :"https://www.adobe.com/eg_en/creativecloud/illustration/discover/learning-to-draw.html"
+    },
+    {
+        name: "Photography", mood: ["happy", "curious", "bored"],
+        description: "Explore your surroundings and learn to capture interesting moments.",
+        img :"https://img.magnific.com/free-photo/photographers-exploring-abandoned-location_23-2150974366.jpg?semt=ais_hybrid&w=740&q=80",
+        benefits :"Creativity, observation and exploring your surroundings ", need:"Camera or a phone",
+        link :"https://www.adobe.com/eg_en/creativecloud/photography/basics.html"
+    },
+    {
+        name: "Running", mood: ["energetic", "stressed"],
+        description: "A simple outdoor activity that helps you stay active and clear your mind.",
+        img :"https://hips.hearstapps.com/hmg-prod/images/868226bd-1882-4a7e-b5dd-3b7b36c7fba2.jpg?crop=0.666666666667xw:1xh;center,top&resize=1200:*",
+        benefits :"Fitness, energy and stress relief", need:"Comfortable clothes",
+        link :"https://www.runnersworld.com/uk/training/beginners/a772727/how-to-start-running-today/"
+    },
+    {
+        name: "Yoga",mood: ["calm", "stressed", "tired"],
+        description: "A relaxing activity that combines movement, breathing, and mindfulness.",
+        img :"https://omsivayoga.com/wp-content/uploads/0x0-1.jpg",
+        benefits :"Relaxation, flexability and body awareness", need:"Comfortable clothes and a yoga mat",
+        link :"https://health.clevelandclinic.org/yoga-for-beginners"
+    },
+    {
+        name: "Chess",mood: ["curious", "bored", "calm"],
+        description: "A strategic game that challenges your mind and improves problem-solving.",
+        img :"https://www.thesprucecrafts.com/thmb/7F787XGs9couW2bOcBorwj6KM0M=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/dv2015029-56a136155f9b58b7d0bd0d71.jpg",
+        benefits :"Problem-solving, concentration and strategic", need:"Chess board or chess app",
+        link :"https://www.chess.com/learn-how-to-play-chess"
+    },
+    {
+        name: "Cooking", mood: ["happy", "bored" , "energetic"],
+        description: "Experiment with ingredients and create something delicious.",
+        img :"https://st2.depositphotos.com/1017986/6607/i/450/depositphotos_66076883-stock-photo-happy-friends-and-chef-cook.jpg",
+        benefits :"Creativity, independence and life skill", need:"Kitchen tools and ingredients",
+        link :"https://www.bbcgoodfood.com/"
+    },
+    {
+        name: "Gardening", mood: ["calm", "stressed", "curious"],
+        description: "Take care of plants and enjoy spending time with nature.",
+        img :"https://img.magnific.com/free-photo/senior-couple-caring-flowers_23-2148256711.jpg?semt=ais_test_b&w=740&q=80",
+        benefits :"Relaxation, patience and connection with nature", need:"Plants, soil, pots and gardening tools",
+        link :"https://www.rhs.org.uk/advice/beginners-guide"
+    },
+    {
+        name: "Journaling",mood: ["calm", "stressed", "curious"],
+        description: "Write down your thoughts, ideas, and experiences.",
+        img :"https://static.vecteezy.com/system/resources/thumbnails/074/414/007/small/a-notebook-pen-and-cup-of-tea-on-a-desk-photo.jpg",
+        benefits :"Self-reflection, organization and creativity", need:"A notebook and a pen",
+        link :"https://positivepsychology.com/how-to-start-journaling/"
+    },
+    {
+        name :"Reading" , mood : ["calm" ,"curious" , "bored"],
+        description :"Exploring books, novels, poetry, and ideas to discover new worlds and perspectives.",
+        img :"https://images.unsplash.com/photo-1532294220147-279399e4e00f?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fHBlcnNvbiUyMHJlYWRpbmd8ZW58MHx8MHx8fDA%3D",
+        benefits :"Knowledge, imagination and concentration", need:"A book",
+        link :"https://bookriot.com/how-to-start-reading/"
+    },
+    {
+        name :"Fitness" , mood :["energetic" ,"happy" ,"stressed"],
+        description :"Exercising through activities such as walking, running, swimming, or strength training.",
+        img :"https://static.independent.co.uk/s3fs-public/thumbnails/image/2017/05/06/14/istock-500671490.jpg",
+        benefits :"Strength, energy and physical fitness", need:"Comfortable clothes and enough space",
+        link :"https://www.acefitness.org/resources/everyone/exercise-library/experience/beginner/?srsltid=AfmBOoo40umZ1dT_8kNOblOOWSUa9DImESXdm1LeuwgsQN9h7rcPBU4P"
+    }
+];
---
+++ script.js
@@ -0,0 +1,79 @@
+let name = document.querySelector("#name");
+let button = document.querySelector("#start");
+let happy = document.querySelector("#happy");
+let calm = document.querySelector("#calm");
+let energrtic = document.querySelector("#energetic");
+let bored = document.querySelector("#bored");
+let curious = document.querySelector("#curious");
+let stressed = document.querySelector("#stressed");
+let result = document.querySelector("#result");
+let mood = document.querySelector("#mood");
+let greet = document.querySelector("#greeting");
+
+
+button.addEventListener("click",function () {
+    let input = name.value;
+    if(input.length < 3){
+        alert("Please enter a valid name with at least 3 letters")
+        return;
+    }
+ greet.innerHTML = "Hello " + name.value + ", how are you feeling today?"
+ document.querySelector("#welcome").style.display="block";
+    document.querySelector("#mood").style.display = "block";
+    });
+
+    function show(mood){
+         document.querySelector("#welcome").style.display="none";
+        result.innerHTML ="";
+        result.innerHTML = "<h1>" + mood + "</h1>"
+     for(let i =0 ; i < hobbies.length ; i++){
+        for( let j =0; j <hobbies[i].mood.length ; j++){
+            if(hobbies[i].mood[j] === mood ){
+          result.innerHTML += `
+
+                   <div class="info">
+                   <div class="text">
+                        <h3>` + hobbies[i].name + `</h3>
+                        <p>` + hobbies[i].description + `</p>
+                        <p><b>Benefits : </b>` + hobbies[i].benefits + `</p> 
+                        <p><b>Need : </b>` + hobbies[i].need + `</p>
+                       <p><a href="` + hobbies[i].link + `" target="_blank">Learn more</a></p>
+                       </div>
+                       <img src= "` + hobbies[i].img + `" width="200">
+                </div>
+                 <hr>
+                `;
+            }
+            }
+        }
+    }
+    happy.addEventListener("click" , function(){
+    show("happy");
+     document.body.style.backgroundColor =" #fff3e0";
+    document.body.style.color = "#e65100" ;
+    });
+    calm.addEventListener("click" , function(){
+         show("calm");
+          document.body.style.backgroundColor =" #e0f7fa";
+    document.body.style.color = "#006064" ;
+    });
+    energetic.addEventListener("click" , function(){
+         show("energetic");  
+          document.body.style.backgroundColor ="#e8f8f5";
+    document.body.style.color = "#00695c" ;
+    });
+    curious.addEventListener("click" , function(){
+         show("curious");
+          document.body.style.backgroundColor = "#f3e5f5";
+    document.body.style.color =  "#4a148c" ;
+    });
+    stressed.addEventListener("click" , function(){
+         show("stressed");
+          document.body.style.backgroundColor ="#ffebee";
+    document.body.style.color = "#c62828"; 
+    });
+    bored.addEventListener("click" , function(){
+         show("bored");
+          document.body.style.backgroundColor ="#f8f4f0"; 
+    document.body.style.color = "#4a3528"; 
+    });
